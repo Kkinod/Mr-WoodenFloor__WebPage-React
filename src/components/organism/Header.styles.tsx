b8b16f00074e661c5156acd3d7651fcb8e76375c
Kamil <naszewlasnepl@gmail.com>
2023-01-10 08:02:38 +0100

    test commit

--- src/components/organism/Header.styles.tsx
+++ src/components/organism/Header.styles.tsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
-interface IButtonProps {
-    isSecondary?: boolean;
-    height?: string;
-}
+// interface IButtonProps {
+//     isSecondary?: boolean;
+//     height?: string;
+// }
 
-export const Wrapper = styled.div`
-    background-color: ${({ isSecondary }: IButtonProps): string => (isSecondary ? 'red' : 'black')};
-    height: ${({ height }: IButtonProps): string => height || '100vh'};
-`;
+// export const Wrapper = styled.div`
+//     background-color: ${({ isSecondary }: IButtonProps): string => (isSecondary ? 'red' : 'black')};
+//     height: ${({ height }: IButtonProps): string => height || '100vh'};
+// `;
