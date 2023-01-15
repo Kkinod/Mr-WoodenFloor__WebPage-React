0369f93b5fad0f4fec3ae6ba1f7dcf0e5392eae1
Kamil <kamil.p@tutanota.com>
2023-01-15 16:47:31 +0100

    add Absolute Imports

--- src/components/organism/Header.tsx
+++ src/components/organism/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BgVideContent, BgVideo, Wrapper } from './Header.styles';
-import videoHeaderMp4 from '../../assets/video/CuttingWoodmp4.mp4'
-import videoHeaderWebm from '../../assets/video/CuttingWoodWebm.webm'
+import videoHeaderMp4 from 'assets/video/CuttingWoodmp4.mp4';
+import videoHeaderWebm from 'assets/video/CuttingWoodWebm.webm';
 
 export const Header = (): JSX.Element => {
     return (
