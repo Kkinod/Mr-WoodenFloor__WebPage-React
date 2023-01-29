8b00a55d98f69569db9b59e3476960b730ac836e
Kamil <kamil.p@tutanota.com>
2023-01-29 13:54:23 +0100

    add labels file with labels

--- src/components/organism/Navigation/Navigation.tsx
+++ src/components/organism/Navigation/Navigation.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
+import { labels } from 'views/labels';
 import ListItem from 'components/atoms/ListItem/ListItem';
 import NavBackground from 'components/atoms/NavBackground/NavBackground';
 import NavigationLink from 'components/atoms/NavigationLink/NavigationLink';
 import NavList from 'components/atoms/NavList/NavList';
 import BurgerBarsContainer from 'components/molecules/BurgerBarsContainer/BurgerBarsContainer';
-import React from 'react';
 import { NavigationContainer, NavigationWrapper } from './Navigation.styles';
 
 const Navigation = (): JSX.Element => {
@@ -11,23 +12,22 @@ const Navigation = (): JSX.Element => {
         <NavigationContainer>
             <BurgerBarsContainer />
             <NavBackground />
-
             <NavigationWrapper>
                 <NavList>
                     <ListItem>
-                        <NavigationLink href="#section-about">O Nas</NavigationLink>
+                        <NavigationLink href="#section-about">{labels.navigationList.aboutUs}</NavigationLink>
                     </ListItem>
                     <ListItem>
-                        <NavigationLink href="#section-about">Współpraca</NavigationLink>
+                        <NavigationLink href="#section-about">{labels.navigationList.cooperation}</NavigationLink>
                     </ListItem>
                     <ListItem>
-                        <NavigationLink href="#section-about">Wybrane Projekty</NavigationLink>
+                        <NavigationLink href="#section-about">{labels.navigationList.selectedProjects}</NavigationLink>
                     </ListItem>
                     <ListItem>
-                        <NavigationLink href="#section-about">Opinie Klientów</NavigationLink>
+                        <NavigationLink href="#section-about">{labels.navigationList.customersOpinion}</NavigationLink>
                     </ListItem>
                     <ListItem>
-                        <NavigationLink href="#section-about">Napisz do Nas</NavigationLink>
+                        <NavigationLink href="#section-about">{labels.navigationList.writeToUs}</NavigationLink>
                     </ListItem>
                 </NavList>
             </NavigationWrapper>
---
+++ src/views/labels.tsx
@@ -0,0 +1,9 @@
+export const labels = {
+    navigationList: {
+        aboutUs: 'O Nas',
+        cooperation: 'Współpraca',
+        selectedProjects: 'Wybrane Projekty',
+        customersOpinion: 'Opinie klientów',
+        writeToUs: 'Napisz do Nas',
+    }
+}
