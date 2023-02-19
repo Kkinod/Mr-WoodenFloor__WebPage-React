1e788fc72f78404639b94d93b9ee2580d6967f8f
Kamil <kamil.p@tutanota.com>
2023-02-19 11:23:17 +0100

    install types react-slick

--- src/components/organism/SectionReviews/SectionReviews.tsx
+++ src/components/organism/SectionReviews/SectionReviews.tsx
@@ -3,31 +3,16 @@ import CenterHeadingSecondary from 'components/molecules/CenterHeadingSecondary/
 import React, { useEffect, useRef } from 'react';
 import { labels } from 'views/labels';
 import { ReviewsContainer, Section_Reviews, ShadowBg } from './SectionReviews.stylest';
-import $ from 'jquery';
+import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-
 const SectionReviews = (): JSX.Element => {
-    const sliderRef = useRef<HTMLDivElement | null>(null);
-
-    useEffect(() => {
-        if (sliderRef.current) {
-            $(sliderRef.current).slick({
-                dots: true,
-                infinite: true,
-                speed: 500,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            });
-        }
-    }, [sliderRef]);
-
     return (
         <Section_Reviews id="section-reviews">
             <ShadowBg />
             <CenterHeadingSecondary headingText={labels.sectionReviews.heading} />
-            <ReviewsContainer ref={sliderRef}>
+            <ReviewsContainer>
                 <CardCustomerReview />
                 <CardCustomerReview />
             </ReviewsContainer>
