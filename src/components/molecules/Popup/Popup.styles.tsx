3b50330284c681caee3655339a86f6feee31b410
Kamil <kamil.p@tutanota.com>
2023-03-04 19:31:35 +0100

    add popup container

--- src/components/molecules/Popup/Popup.styles.tsx
+++ src/components/molecules/Popup/Popup.styles.tsx
@@ -1,1 +1,15 @@
 import styled from 'styled-components/macro';
+import { ITheme } from 'assets/styles/theme';
+
+export const PopupContainer = styled.div`
+    position: fixed;
+    z-index: 9999;
+    width: 100%;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    background-color: rgba(${({ theme }: ITheme): string => theme.colors.blackRgb}, 0.8);
+    opacity: 0;
+    visibility: hidden;
+    transition: all 0.3s;
+`;
