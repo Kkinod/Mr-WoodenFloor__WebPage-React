21466f7d75caf32014d42827a4d76e16f5e3d4b4
Kamil <kamil.p@tutanota.com>
2023-04-04 20:11:29 +0200

    increased maximum height of cards in a section features

--- src/components/atoms/CardsComponents/Card.styles.tsx
+++ src/components/atoms/CardsComponents/Card.styles.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components/macro';
 export const Card = styled.div`
     position: relative;
     width: 50%;
-    max-height: 320px;
+    max-height: 450px;
     overflow: hidden;
     text-align: center;
 `;
